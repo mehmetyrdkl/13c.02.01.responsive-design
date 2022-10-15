6faa2077d339010511287055c0763e783995fa65
Mehmet <mehmet.h.yurdakul@gmail.com>
2022-10-15 14:40:24 +0200

    product-section-done

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 import { inView, animate, scroll } from "https://cdn.skypack.dev/motion";
 
-document.querySelectorAll("section").forEach((item) => {
-  scroll(animate(item, { opacity: [0, 1, 1, 0] }), {
-    target: item,
-    offset: ["start end", "end end", "start start", "end start"],
-  });
+inView("body", ({ target }) => {
+  animate(
+    target.querySelector(".fade"),
+    { opacity: 1, x: [-70, -50, 0] },
+    { delay: 0.2, duration: 0.9, easing: [0.17, 0.55, 0.55, 1] }
+  );
 });
 
 const toggleButton = document.getElementsByClassName("toggle-button-nav")[0];
